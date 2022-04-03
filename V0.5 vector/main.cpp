e0c1ac8a1591daa0837a591da1b76d2acbae579b
norbertas156 <55793789+norbertas156@users.noreply.github.com>
2022-04-03 20:03:48 +0300

    Add files via upload

---
+++ V0.5 vector/main.cpp
@@ -0,0 +1,59 @@
+#include "Papildyniai/Library.h"
+#include "Papildyniai/Prototipai.h"
+#include "Papildyniai/MainPrototipai.h"
+
+
+int main(){
+
+  vector <studentai> student;
+  vector <studentai> kietekai;
+  vector <studentai> nelaimingieji;
+
+  char tikrinimas;
+  cout<<"Ar studentai bus rasomi ranka ar skaitomi is failo ? Iveskite: (i-ivedami, s-skaitomi is failo )"<<endl;
+  char raide = RaidesTikrinimas('i','s');
+if(raide == 'i'){
+  while(tikrinimas != 'n'){
+  studentai stud;
+  Ivedimas(stud);
+  student.push_back(stud);
+  cout<<"Ar dar bus studentu ? Iveskite: (t-taip, n-ne)"<<endl;
+  tikrinimas = RaidesTikrinimas('t','n');
+  if(tikrinimas == 'n')
+  break;
+    }
+  }
+  else if(raide == 's'){
+      cout<<"Ar norite, kad studentu failas butu sugeneruotas. Iveskite: (t-taip, n-ne)"<<endl;
+      raide = RaidesTikrinimas('t','n');
+      if(raide == 'n'){
+        SkaitymasIsFailo(student);
+      } else{
+
+            cout<<"Iveskite studentu skaiciu"<<endl;
+            int sk = SkaiciausTikrinimams();
+            AtsitiktinisFailas(sk); 
+            SkaitymasIsFailo(student);
+      }
+};
+
+// auto st2 = std::chrono::high_resolution_clock::now();
+// std::sort(student.begin(), student.end(),[](studentai a, studentai b){
+//   return a.vardas < b.vardas;
+//   });
+//   std::chrono::duration<double> df2 = std::chrono::high_resolution_clock::now()-st2; // Skirtumas (s)
+//   std::cout << "Failo surusiavimas uzruko: "<< df2.count() << " s\n";
+
+    auto st3 = std::chrono::high_resolution_clock::now();
+    Paskirstymas(student,kietekai,nelaimingieji);
+    std::chrono::duration<double> df3 = std::chrono::high_resolution_clock::now()-st3; // Skirtumas (s)
+     std::cout << "Studentu paskirstymas i kietekus ir nelaiminguosisu uztruko: "<< df3.count() << " s\n";
+
+
+    IsvedimasIFaila(kietekai,"kietekai");
+    IsvedimasIFaila(nelaimingieji,"nelaimingieji");
+
+      VisasLaikas += df3.count();
+     cout<<"Viso sugaista laiko: "<< VisasLaikas<<endl;
+
+}
