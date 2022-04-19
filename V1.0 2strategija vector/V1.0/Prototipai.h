1dd4e42822cd84c08bdaa84749daca7b379cc4ba
norbertas156 <55793789+norbertas156@users.noreply.github.com>
2022-04-19 19:18:30 +0300

    Add files via upload

---
+++ V1.0 2strategija vector/V1.0/Prototipai.h
@@ -0,0 +1,17 @@
+#ifndef Prototipai_H_INCLUDED
+#define Prototipai_H_INCLUDED
+
+//#include "Library.h"
+#include "Tikrinimai.cpp"
+#include "VidMed.cpp"
+
+double Vidurkis(studentai & stud);
+double Mediana(studentai& stud);
+char RezultatuTikrinimas();
+char RaidesTikrinimas(char x, char y);
+int SkaiciausTikrinimams();
+int PazymioTikrinimas();
+void SkaitymasIsFailo();
+void Paskirstymas (vector<studentai> &student, vector<studentai> &kietekai,vector<studentai> &nelaimingieji);
+
+#endif
